44499cf1bbccbd5d289cce7d4e26cdc7bb482f75
ReindeerSwain <1208532105@qq.com>
2020-10-23 17:16:37 +0800

    add 二元一次

--- 2Y1C.cpp
+++ 2Y1C.cpp
@@ -1,28 +1,29 @@
 #include <stdio.h>
-
+#include <stdlib.h> 
 int main(void)
 //ax + by =m
 //cx + dy =n
 
 {
-	int a, b, c, d, m, n, x, y;
+float a, b, c, d, m, n, x, y;
 	printf("ax + by =m\n");
 	printf("cx +dy =n\n");
 	printf("请输入二元一次方程的参数：");
 	printf("a:");
-	scanf("%d",&a);
+	scanf("%f",&a);
 	printf("b:");
-	scanf("%d",&b);
+	scanf("%f",&b);
 	printf("c:");
-	scanf("%d",&c);
+	scanf("%f",&c);
 	printf("d:");
-	scanf("%d",&d);
+	scanf("%f",&d);
 	printf("m:");
-	scanf("%d",&m);
+	scanf("%f",&m);
 	printf("n:");
-	scanf("%d",&n);
+	scanf("%f",&n);
 	x = (b*n - d*m)/(b*c - a*d);
 	y = (c*m - a*n)/(b*c - a*d);
-	printf("x=%d y=%d\n",x,y);
+	printf("x=%f y=%f\n",x,y);
+	system("pause");
 	return 0;
 }
