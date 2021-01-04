93d88cff34afaa9b9988c20510435b4d3c20216f
ReindeerSwain <1208532105@qq.com>
2021-01-04 22:46:43 +0800

    oj exercise

--- oj4/oj4C.cpp
+++ oj4/oj4C.cpp
@@ -2,17 +2,62 @@
 #include <string.h>
 
 int main(){
-	int n;
-	char a[200];
+	int len,n,x=0,y=0,j,m,p=0;
+	char a[200],odd[200],even[200];
+	
 	scanf("%d",&n);
+	getchar();
 	for(int i=0;i<n;i++){
-		getchar();
 		fgets(a,200,stdin);
-		int len;
 		len=strlen(a)-1;
-		for(int j=0;j<len;j+=2){
-			if(a[j]%2!=0)
-			b[]
+		for(j=0;j<len;j++){
+			if(a[j]==' '||a[j]=='\n'){
+				for(m=p;m<j;m++){
+					if(a[m]%2==0)
+				{even[x]=a[m];
+				x++;
+				}
+				else if(a[m]%2!=0) {
+					odd[y]=a[m];
+					y++;
+				}
+				p=j;
+				}
+				
+				
+			}
 		}
+		int len1,len2,t,k;
+		len1=strlen(odd);
+		len2=strlen(even);
+		for(j=0;j<len1-1;j++){
+			for(k=0;k<len1-j-1;k++){
+				if(odd[k]>odd[k+1]){
+					t=odd[k];
+					odd[k]=odd[k+1];
+					odd[k+1]=t;
+				}
+			}
+		}
+		for(j=0;j<len2-1;j++){
+			for(k=0;k<len2-j-1;k++){
+				if(even[k]>even[k+1]){
+					t=even[k];
+					even[k]=even[k+1];
+					even[k+1]=t;
+				}
+			}
+		}
+		for(j=0;j<len1;j++)
+		 printf("%c ",odd[j]);
+		for(j=0;j<len2-1;j++) 
+		 printf("%c ",even[j]); 
+		printf("%c\n",even[len2-1]);
+		x=0;
+		y=0;
+		for(j=0;j<len1;j++)
+		 odd[j]=0;
+		for(j=0;j<len2;j++)
+		 even[j]=0;
 	}
 }
---
+++ oj7/oj7a.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+	int sum,num[15];
+	for(int i=0;i<10;i++){
+		scanf("%d",&num[i]);
+		sum=sum+num[i];
+	}
+	
+	int a[200];
+	int j=0,sum0=0;
+	for(int i=0;i<10;i++){
+		for(;j<(sum0+num[i]);j++){
+			a[j]=i;
+		}
+		sum0=sum0+num[i];
+		j=sum0;
+	}
+	
+	int t,i,n=0;
+	for(i=0;i<sum-1;i++){
+	for(j=0;j<(sum-i-1);j++){
+		if(a[j]>a[j+1]){
+			t=a[j];
+			a[j]=a[j+1];
+			a[j+1]=t;
+		}
+	}	
+	}
+	if(a[0]==0){
+	    for(i=0;i<sum;i++){
+		if(a[i]!=0){
+			a[0]=a[i];
+			a[i]=0;
+			break;
+		}
+	}
+	}
+
+	for(i=0;i<sum;i++)
+	  printf("%d",a[i]);
+
+}
